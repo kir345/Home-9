795c537d46c7dc38f088166b44068d7c1f9e2494
kir345 <123649165+kir345@users.noreply.github.com>
2023-03-14 12:58:04 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,28 +1,30 @@
-﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. 
-//Даны два неотрицательных числа m и n.
-//m = 2, n = 3 -> A(m,n) = 9
-//m = 3, n = 2 -> A(m,n) = 29
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в 
+//промежутке от N до 1. Выполнить с помощью рекурсии.
+//N = 5 -> "5, 4, 3, 2, 1"
+//N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
 using System;
 using static System.Console;
 
 Clear();
-int m = InputNum("Введите m: ");
-int n = InputNum("Введите n: ");
-int A = Ack(m, n);
+Write("Введите число N: ");
+int n = int.Parse(ReadLine());
 
-Write($"Функция Аккермана = {A} ");
-
-int Ack(int m, int n)
+for (int i = n; i > 0; i-- )
 {
-  if (m == 0) return n + 1;
-  else if (n == 0) return Ack(m - 1, 1);
-  else return Ack(m - 1, Ack(m, n - 1));
+    Write($"{i} ");
 }
+WriteLine();
+WriteLine(PrintNums(n));
 
-int InputNum(string input) 
+string PrintNums(int n)
 {
-  Write(input);
-  int output = Convert.ToInt32(ReadLine());
-  return output;
+    if (n == 5)
+    {
+        WriteLine();
+        return "1";
+    }
+    string s = PrintNums(n + 1) + " " + n.ToString();
+    WriteLine(s);
+    return s;
 }
