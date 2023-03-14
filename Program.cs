50d137da024b2d974382cddf03388fe4664ab1c0
kir345 <123649165+kir345@users.noreply.github.com>
2023-03-14 12:52:29 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов 
+//в промежутке от M до N.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+
+using System;
+using static System.Console;
+
+Clear();
+WriteLine("Введите значение M: ");
+int numM = int.Parse(ReadLine());
+
+WriteLine("Введите значение N: ");
+int numN = int.Parse(ReadLine());
+
+void GapNumSum (int numM, int numN, int sum)
+{
+    if (numM > numN)
+    {
+        WriteLine($"-> {sum}");
+        return;
+    }
+    sum = sum + (numM++);
+    GapNumSum (numM, numN, sum);
+}
+GapNumSum(numM, numN, 0);
+
+
+
+
